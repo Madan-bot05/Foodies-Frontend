f90697bdbebf36050f47434e229fb00800ec0a4e
Madan-bot05 <madanmohandas983@gmail.com>
2024-07-31 19:10:43 +0530

    Started working on navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className=''>Navbar
+            <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default Navbar
