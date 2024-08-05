cda3e1f57bdeb90c1a1e1a61484518ca11634eb3
Madan-bot05 <madanmohandas983@gmail.com>
2024-08-05 19:32:51 +0530

    Completed The homepage

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.css'
+import gsap from 'gsap';
 
 function Home() {
+
+  useEffect(() => {
+    gsap.to("#taste", { opacity: 1, x: 360, duration: 2 });
+  }, []);
+  useEffect(() => {
+    gsap.to("#taste1", { opacity: 1, x:-360, duration: 2 });
+  }, []);
+
+  useEffect(() => {
+   gsap.to("#name", { rotation: 180, x: -500, duration: 5 ,color:"rgb(230,3,99)"});
+  }, []);
+  useEffect(() => {
+   gsap.to("#name1", { rotation: 180, x: -500, duration: 5 ,color:"black"});
+  }, []);
+
   return (
     <div className=''>
       <section className='banner -z-50 relative flex flex-col justify-center items-center'>
-        <div className='w-[50vw] z-10 text-center'>
-          <p className='text-2xl lg:text-6xl font-bold z-10 py-5'>Madan Foods</p>
-          <p className='z-10 text-gray-700 text-xl lg:text-4xl'>Taste the Convience: Food, Fast and Delivered</p>
+        <div  className='w-[50vw] z-10 text-center'>
+          <span id='name' className=' text-2xl lg:text-6xl font-bold z-10 py-5 font-black'>Madan <span id='name1' className='food text-pink-700'>Foods</span> </span>
+          {/* <p id='taste' className=' z-10 text-gray-700 text-xl lg:text-4xl'>Taste the Convience: <span id='taste1' className='text-pink-700'> Food, Fast and Delivered</span></p> */}
+          <div className="flex items-stretch ">
+        <div id='taste' className=' z-10 text-gray-700 text-xl lg:text-4xl'> Food, Fast and Delivered</div>
+        <div id='taste1' className=' z-10  text-xl lg:text-4xl text-orange-300'> Taste the Convience:</div>
+      </div>
+
+
+
         </div>
 
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
         gsap.to("#box2", { x: -50, duration: 1 });  // Corrected the selector
       }, []);
   return (
-    <div className='px-5 z-50 py-[0.8rem] bg-[#e91e63] lg:px-20 flex justify-between shadow-sm'>
+    <div className='px-5 z-50 py-[0.8rem] bg-orange-800 lg:px-20 flex justify-between shadow-sm'>
         
             <div className='lg:mr-10 cursor-pointer flex items-center space-x-4'>
                 <span id='box' className='logo font-semibold text-gray-300 text-2xl'>
