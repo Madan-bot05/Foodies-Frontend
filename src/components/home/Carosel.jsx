8e1441362450a344252e6c494ae010e39e0aaf10
Madan-bot05 <madanmohandas983@gmail.com>
2024-08-05 20:12:59 +0530

    working on carousel part

--- src/components/home/Carosel.jsx
+++ src/components/home/Carosel.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick'
+import { topMeal } from './topMeals';
+import CaroselItem from './CaroselItem';
 
 function Carosel() {
   return (
-    <div>Carosel</div>
+    <div>
+        <Slider>
+            {topMeal.map((item)=><CaroselItem image={item.image} title={item.title}/>)}
+        </Slider>
+    </div>
   )
 }
 
--- src/components/home/topMeals.js
+++ src/components/home/topMeals.js
@@ -1,4 +1,4 @@
-const topMeal=[
+export const topMeal=[
     {
         image:"https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg",
         title:"pizza"
